4c666cb92bf09a9636cc6949a2bb4d91372ffcdf
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-03 02:36:03 +0530

    Initial commit

---
+++ src/main/java/com/example/dancer/DancerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dancer/DancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
