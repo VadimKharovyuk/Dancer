c5bfaa0588c4d4272de13bca10a0a9df753069bd
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-03 02:36:40 +0530

    change

--- src/main/java/com/example/dancer/DancerApplication.java
+++ src/main/java/com/example/dancer/DancerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DancerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DancerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DancerApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/dancer/DancerApplicationTests.java
+++ src/test/java/com/example/dancer/DancerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DancerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
